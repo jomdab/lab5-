89caf8f070a60b8034406a1d26cd3e87227cca35
jomdab <shupak999@gmail.com>
2020-12-15 17:27:11 +0800

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
+    int x=12,e=0,o=0;
+ while(x!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    if(x%2==0 && x!=0){
+        e++;
+    } 
+    else if(x%2!=0 && x!=0){
+        o++;
+    }
+ }
+    cout << "#Even numbers = "<< e << "\n0";
+    cout << "#Odd numbers = " << o;
     return 0;
 }
